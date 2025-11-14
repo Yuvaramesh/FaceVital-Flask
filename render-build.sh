f34b5f69623d77f9298a2a1195b50d140c47f137
Yuvaramesh <yuvasri1102003@gmail.com>
2025-11-14 23:46:38 +0530

    Fix project structure and make build script executable

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 # Install system dependencies
 apt-get update
 apt-get install -y gfortran libopenblas-dev liblapack-dev
 
-# Install Python packages
+# Upgrade pip
 pip install --upgrade pip
+
+# Install Python dependencies
 pip install -r requirements.txt
