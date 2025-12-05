c6588071edeb71c9037ebe822c9bce0300d9ab08
Yuvaramesh <yuvasri1102003@gmail.com>
2025-12-05 13:21:33 +0530

    Feat: Testing

--- app.py
+++ app.py
@@ -681,5 +681,5 @@ def generate_report(session_id):
         return jsonify({"success": False, "error": str(e)})
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
